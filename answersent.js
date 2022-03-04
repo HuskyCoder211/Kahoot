1f497ac00d24f6c8a11893cdd2f7d38b45300541
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2022-03-04 11:37:05 -0500

    Update answersent.js

--- answersent.js
+++ answersent.js
@@ -11,7 +11,28 @@
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                       <link rel="preconnect" href="https://cdn.amplitude.com"/>
                         <title>Play Kahoot! - Enter game PIN here!</title>
-<script>function sendGameStats(){}!function(e,t){var n=e.amplitude||{_q:[],_iq:{}},i=t.createElement("script");i.type="text/javascript",i.integrity="sha384-a+mq7tiLwde/00Oc7avFHLn/ttGfdAq1rtZc7u97SEzIiyYoT2IsOKWCkAThwdEu",i.crossOrigin="anonymous",i.async=!0,i.src="https://cdn.amplitude.com/libs/amplitude-5.3.0-min.gz.js",i.onload=function(){e.amplitude.runQueuedFunctions||console.log("[Amplitude] Error: could not load SDK")};var r=t.getElementsByTagName("script")[0];function s(e,t){e.prototype[t]=function(){return this._q.push([t].concat(Array.prototype.slice.call(arguments,0))),this}}r.parentNode.insertBefore(i,r);for(var o=function(){return this._q=[],this},a=["add","append","clearAll","prepend","set","setOnce","unset"],u=0;u<a.length;u++)s(o,a[u]);n.Identify=o;for(var c=function(){return this._q=[],this},l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"],p=0;p<l.length;p++)s(c,l[p]);n.Revenue=c;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"];function f(e){function t(t){e[t]=function(){e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}for(var n=0;n<d.length;n++)t(d[n])}f(n),n.getInstance=function(e){return e=(e&&0!==e.length?e:"$default_instance").toLowerCase(),n._iq.hasOwnProperty(e)||(n._iq[e]={_q:[]},f(n._iq[e])),n._iq[e]},e.amplitude=n}(window,document)</script></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script src="//assets-cdn.kahoot.it/controller/v2/assets/js/runtime-main.086629cd.js"></script><script src="//assets-cdn.kahoot.it/controller/v2/assets/js/vendors~main.62100167.chunk.js"></script>
+<script>
+                          function sendGameStats(){}!function(e,t){
+                          var n=e.amplitude||{_q:[],_iq:{}},i=t.createElement("script");
+                          i.type="text/javascript",
+                            i.integrity="sha384-a+mq7tiLwde/00Oc7avFHLn/ttGfdAq1rtZc7u97SEzIiyYoT2IsOKWCkAThwdEu",
+                            i.crossOrigin="anonymous",
+                            i.async=!0,
+                            i.src="https://cdn.amplitude.com/libs/amplitude-5.3.0-min.gz.js",
+                            i.onload=function(){
+                            e.amplitude.runQueuedFunctions||console.log("[Amplitude] Error: could not load SDK")
+                          };
+                          var r=t.getElementsByTagName("script")[0];
+                          function s(e,t){
+                            e.prototype[t]=function(){
+                            return this._q.push([t].concat(Array.prototype.slice.call(arguments,0))),this}r.parentNode.insertBefore(i,r);
+                          for(var o=function(){return this._q=[],this},a=["add","append","clearAll","prepend","set","setOnce","unset"],u=0;u<a.length;u++)s(o,a[u]);n.Identify=o;
+                          for(var c=function(){return this._q=[],this},l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"],p=0;p<l.length;p++)s(c,l[p]);n.Revenue=c;
+                            var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"];
+                            function f(e){
+                              function t(t){e[t]=function(){
+                                e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}for(var n=0;n<d.length;n++)t(d[n])}f(n),n.getInstance=function(e){return e=(e&&0!==e.length?e:"$default_instance").toLowerCase(),n._iq.hasOwnProperty(e)||(n._iq[e]={_q:[]},f(n._iq[e])),n._iq[e]},e.amplitude=n}(window,document)
+                          </script></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script src="//assets-cdn.kahoot.it/controller/v2/assets/js/runtime-main.086629cd.js"></script><script src="//assets-cdn.kahoot.it/controller/v2/assets/js/vendors~main.62100167.chunk.js"></script>
 <script src="//assets-cdn.kahoot.it/controller/v2/assets/js/main.39d3c151.chunk.js"></script>
 </body>
 </html>
